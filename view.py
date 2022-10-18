65841525b65f0d346c5490d776cda1bde67da986
Karina <108223541+KarinaKazamanova@users.noreply.github.com>
2022-10-18 09:15:56 +0300

    Update view.py

--- view.py
+++ view.py
@@ -1,13 +1,13 @@
 def get_name():
     with open('file.txt', 'r') as f:
-        return str(f.readline()[1:2]) # пока не поняла, верно ли писать так
+        return str(f.readlines()[1:2]) # пока не поняла, верно ли писать так
 
 def get_number():
     with open('file.txt', 'r') as f:
-        return str(f.readline()[2:3])
+        return str(f.readlines()[2:3])
 
 def what_to_do():
     with open('file.txt', 'r') as f:
-        return str(f.readline()[3:])
+        return str(f.readlines()[3:])
 
 
